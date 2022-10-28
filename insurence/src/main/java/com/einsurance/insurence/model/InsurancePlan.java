8b15f417f7b670f151cdb26e165108c9fda221c0
Rama Krishna <ramudu714@gmail.com>
2022-10-28 18:45:36 +0530

    plan -rk

--- insurence/src/main/java/com/einsurance/insurence/model/InsurancePlan.java
+++ insurence/src/main/java/com/einsurance/insurence/model/InsurancePlan.java
@@ -1,12 +1,29 @@
 package com.einsurance.insurence.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class InsurancePlan {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long insurancePlanId;
-	private String name;
+	
+	private long InsuranceTypeId;
+	private long InsuranceSchemeId;
+	private int minumumPolicyTerm;
+	private int maximumPolicyTerm;
+	private int minimumAge;
+	private int maximumAge;
+	private double minimumInvestmentAmount;
+	private double maximumInvestmentAmount;
+	private double profitRatio;
+	private String status;
+
 }
