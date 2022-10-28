0f0a420755b19245bd10b25cf84bc1784efcb1c1
Rama Krishna <ramudu714@gmail.com>
2022-10-28 18:16:20 +0530

    rk

---
+++ insurence/src/main/java/com/einsurance/insurence/model/InsurancePlan.java
@@ -0,0 +1,11 @@
+package com.einsurance.insurence.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class InsurancePlan {
+
+	@Id
+	private long insurancePlanId;
+}
