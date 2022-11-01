ca8f103bc567aafa98c2b0a9619aafc76628522c
ghosh <ghosh@Subhranil>
2022-11-01 23:11:41 +0530

    sd

--- insurence/src/main/java/com/einsurance/insurence/controller/AdminController.java
+++ insurence/src/main/java/com/einsurance/insurence/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,7 +40,7 @@ import com.einsurance.insurence.service.InsuranceSchemeService;
 import com.einsurance.insurence.service.InsuranceSettingsService;
 import com.einsurance.insurence.service.InsuranceTypeService;
 import com.einsurance.insurence.service.StateService;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
